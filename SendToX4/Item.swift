7658f737a2d0a776fbe804e880cdd82ffd70d95c
Jonathan Taveras <jonathanrtv@outlook.com>
2026-02-13 21:31:24 -0500

    Initial Commit

---
+++ SendToX4/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SendToX4
+//
+//  Created by Jonathan Taveras Vargas on 2/13/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
